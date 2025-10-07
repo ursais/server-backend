47b52edb3d8ce9728a58658d20469a6ea2ee7264
dbrogan-osi <dbrogan@opensourceintegrators.com>
2025-10-07 20:03:02 +0000

    Fix for ticket 67095: Submodule update
    Automated changes for ticket resolution

--- base_dav/models/__init__.py
+++
@@ -1,4 +0,0 @@
-# Copyright 2018 Therp BV <https://therp.nl>
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from . import dav_collection
-from . import dav_collection_field_mapping
